87660c96198981ef1ee570721364c10bf97be83b
Wekuz <wekuz@duck.com>
2025-11-22 22:44:54 +0200

    added kernel module

--- hosts/opti/hardware-configuration.nix
+++ hosts/opti/hardware-configuration.nix
@@ -4,7 +4,7 @@
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
   boot.initrd.availableKernelModules =
-    [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" ];
+    [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
